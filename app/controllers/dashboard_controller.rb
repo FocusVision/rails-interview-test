c3dd229ae160619d2a8b0bf4e43d6b0ab6fb72c4
Prakash Sejwani <prakash.sejwani@kalpavruksh.com>
2017-08-18 13:44:08 +0530

    Change dashboard page according to requirements of all users, questions, answers and Tenant API requests count

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
   skip_before_filter :api_authentication
   def index
-    @users = User.includes(:questions, :answers)
+    @users = User.all
+    @questions = Question.all
+    @answers = Answer.all
     @tenants = Tenant.all
   end
 end
