3ab9a9a08c7d725c8021baf79d79d6c1e0b982fa
Prakash Sejwani <prakash.sejwani@kalpavruksh.com>
2017-08-18 15:06:20 +0530

    Fix questions query from concatenation to interpolation

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,11 +4,11 @@ class Question < ActiveRecord::Base
   belongs_to :user, inverse_of: :questions
 
   def self.all_by_query_params(params)
-    questions = self.where(private: false)
+    questions = where(private: false)
     if params[:question_name]
-      questions = questions.where('title LIKE ?', '%' + params[:question_name] + '%')
+      questions = questions.where('title LIKE ?', "%#{params[:question_name]}%")
     elsif params[:answer_name]
-      questions = questions.joins(:answers).where('answers.body LIKE ?', '%' + params[:answer_name] + '%')
+      questions = questions.joins(:answers).where('answers.body LIKE ?', "%#{params[:answer_name]}%")
     else
       questions
     end
