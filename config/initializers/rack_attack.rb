0d0322fef7b6d7e3be7e8566b5d2f17895ec8ff3
Prakash Sejwani <prakash.sejwani@kalpavruksh.com>
2017-08-22 16:03:46 +0530

    Add period from 10 to 10.seconds in rack_attack throttling

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,7 @@
 class Rack::Attack
   # Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new 
 
-  throttle('api/ip', limit: 1, period: 10) do |req|
+  throttle('api/ip', limit: 1, period: 10.seconds) do |req|
     token = req.env['HTTP_AUTHORIZATION'].split('=').last if req.env['HTTP_AUTHORIZATION']
     tenant = Tenant.find_by(api_key: token)
     if tenant
